f71f02282006ad06e590e9071c96c664652a9a32
Michael Wall <mjwall@gmail.com>
2012-11-10 23:10:28 -0500

    Initial commit
    Started with https://github.com/mjwall/scala-maven-sample

---
+++ src/main/scala/com/mjwall/HelloWorld.scala
@@ -0,0 +1,7 @@
+package com.mjwall
+
+object HelloWorld {
+  def main(args: Array[String]) {
+    println("Hello World")
+  }
+}
---
+++ src/test/scala/com/mjwall/HelloWorldSpec.scala
@@ -0,0 +1,16 @@
+package com.mjwall
+
+import org.scalatest.junit.JUnitRunner
+import org.junit.runner.RunWith
+import org.scalatest.FunSpec
+import org.scalatest.matchers.ShouldMatchers
+
+@RunWith(classOf[JUnitRunner])
+class HelloWorldSpec extends FunSpec {
+
+  describe("A HelloWorld") {
+    it ("should return something") (pending)
+
+    it ("should do something else") (pending)
+  }
+}

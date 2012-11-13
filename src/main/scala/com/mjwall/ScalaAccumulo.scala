ba64ba2fd2f71364c4bd86771f88a175b05539d6
Michael Wall <mjwall@gmail.com>
2012-11-12 22:43:47 -0500

    Good start here
    Still needs a lot of work, but I am happy with this.

--- src/main/scala/com/mjwall/ScalaAccumulo.scala
+++ src/main/scala/com/mjwall/ScalaAccumulo.scala
@@ -1,8 +1,86 @@
 package com.mjwall
 
-case class ScalaAccumulo(zookeepers: String, instance: String, username: String, password: String)
+import org.apache.accumulo.core.client.Instance
+import org.apache.accumulo.core.client.ZooKeeperInstance
+
+class ScalaAccumulo(instance: Instance, username: String, password: String) {
+
+  val this.instance: Instance = instance
+  val this.username: String = username
+  val this.password: String = password
+
+  /*
+   * Auxillary constructor that creates a ZookeeperInstance based on the
+   * instanceName and zookeepers string
+   */
+  def this(instanceName: String, zookeepers: String, username: String, password: String) = {
+    this(ScalaAccumulo.getZooKeeperInstance(instanceName, zookeepers), username, password)
+  }
+
+  /*
+   * Auxillary constructor that assumes instance is accumulo, zookeepers are running
+   * on localhost:2181 and the username/password is root/secret
+   */
+  def this() = {
+    this(ScalaAccumulo.getZooKeeperInstance("accumulo","localhost"),"root","secret")
+  }
+
+  def getUsername: String = this.username
+  def getPassword: String = this.password
+  def getInstance: Instance = this.instance
+
+  override def toString: String = "ScalaAccumulo connected to " + instance.getInstanceName + " on " + instance.getZooKeepers + " as " + username
+
+}
+
+case class ZookeeperPair(host: String, port: Int)
 
 object ScalaAccumulo {
-  def apply(): ScalaAccumulo = ScalaAccumulo(null,null,null,null)
+  /*
+   * This method checks that at least one of the zookeeper connections is open before trying to
+   * get Accumulo's ZooKeeperInstance.  Otherwise, the code just loops trying to connect.
+   */
+  def getZooKeeperInstance(instance: String, zookeepers: String): ZooKeeperInstance = {
+    if (parseZookeepers(zookeepers).exists(p => isSocketOpen(p.host, p.port))) {
+      new ZooKeeperInstance(instance, zookeepers)
+    } else {
+      throw new java.net.SocketException("Can't connect to zookeepers with string: " + zookeepers)
+    }
+  }
+
+  def parseZookeepers(zookeepers: String): List[ZookeeperPair] = {
+    if ((null == zookeepers) || (zookeepers == "")) List()
+    else {
+      zookeepers.split(",").map(_.split(":").toList).toList map (_  match {
+        case List(host, port) => ZookeeperPair(host, port.toInt)
+        case List(host) => ZookeeperPair(host, 2181)
+        case Nil => null
+      })
+    }
+  }
+
+  private def isSocketOpen(host: String, port: Int): Boolean = {
+    try {
+      new java.net.Socket(host, port)
+      true
+    } catch {
+      case e: Exception => false
+    }
+  }
+
+  /*
+   * Helper method to return a Mock Accumulo instance
+   */
+  def getMock() = {
+    new ScalaAccumulo(new org.apache.accumulo.core.client.mock.MockInstance(),"root","secret")
+  }
 }
+
+
+
+
+
+
+
+
 
--- src/test/scala/com/mjwall/ScalaAccumuloSpec.scala
+++ src/test/scala/com/mjwall/ScalaAccumuloSpec.scala
@@ -9,28 +9,119 @@ import org.scalatest.matchers.ShouldMatchers
 class ScalaAccumuloSpec extends FunSpec with ShouldMatchers {
 
   describe("ScalaAccumulo") {
-    describe("constructed with empty args") {
-
-      it("should be valid") {
-        ScalaAccumulo()
+    describe("parseZookeepers") {
+      it("should parse one") {
+        val test = ScalaAccumulo.parseZookeepers("localhost:2181")
+        test.head should equal (ZookeeperPair("localhost",2181))
       }
 
-      it("should not have a Connector") {
-        (pending)
+      it("should parse two") {
+        val test = ScalaAccumulo.parseZookeepers("localhost:2181,remotehost:2001")
+        test.head should equal (ZookeeperPair("localhost",2181))
+        test.tail.head should equal (ZookeeperPair("remotehost",2001))
+      }
+
+      it("should default the port to 2181") {
+        val test = ScalaAccumulo.parseZookeepers("localhost:2000,remotehost")
+        test.head should equal (ZookeeperPair("localhost",2000))
+        test.tail.head should equal (ZookeeperPair("remotehost",2181))
+      }
+
+      it("should return an empty list if nothing is passed in") {
+        ScalaAccumulo.parseZookeepers(null) should equal (List())
+      }
+
+      it("should return an empty list if an empty string is passed in") {
+        ScalaAccumulo.parseZookeepers("") should equal (List())
+      }
+
+      it("should still work if there is no , or :") {
+        ScalaAccumulo.parseZookeepers("something").head should equal (ZookeeperPair("something",2181))
       }
     }
 
-    describe("constructed with zookeeper, instance, username and password") {
+    describe("against a live Accumulo") {
+      try {
+        val instanceName = "accumulo"
+        val zookeepers = "localhost"
+        val username = "root"
+        val password = "secret"
+        ScalaAccumulo.getZooKeeperInstance(instanceName, zookeepers)
+        // these tests will only run if you have accumulo running locally
 
-      it("should be valid") {
-        val zk = "zookeeperhere"
-        ScalaAccumulo(zk,"i", "u","p").zookeepers should equal (zk)
-      }
+        describe("constructed with a ZooKeeperInstance, username and password") {
+          val zookeeperInstance = new org.apache.accumulo.core.client.ZooKeeperInstance(instanceName, zookeepers)
+          val scalaAccumulo = new ScalaAccumulo(zookeeperInstance, username, password)
 
-      it("should have a Connector") {
-        (pending)
+          it("should connect to ZooKeeperInstance") {
+            scalaAccumulo.getInstance should equal (zookeeperInstance)
+          }
+
+          it("should use the username") {
+            scalaAccumulo.getUsername should equal (username)
+          }
+
+          it("should use the password"){
+            scalaAccumulo.getPassword should equal (password)
+          }
+        }
+
+        describe("constructed with empty args") {
+          val scalaAccumulo = new ScalaAccumulo()
+
+          it("should connect to localhost") {
+            scalaAccumulo.getInstance match {
+              case i: org.apache.accumulo.core.client.ZooKeeperInstance => {
+                i.getZooKeepers.split(",").head should equal ("localhost")
+              }
+              case _ => fail("Instance was not a ZookeeperInstance")
+            }
+          }
+
+          it("should use 'root' as username") {
+            scalaAccumulo.getUsername should equal ("root")
+          }
+
+          it("should use 'secret' as password") {
+            scalaAccumulo.getPassword should equal ("secret")
+          }
+        }
+
+        describe("constructed with instanceName, zookeepers, username and password") {
+          val scalaAccumulo = new ScalaAccumulo(instanceName, zookeepers, username, password)
+
+          it("should connect to instanceName") {
+            scalaAccumulo.getInstance.getInstanceName should equal (instanceName)
+          }
+
+          it("should connect to zookeepers") {
+            scalaAccumulo.getInstance match {
+              case i: org.apache.accumulo.core.client.ZooKeeperInstance => {
+                i.getZooKeepers.split(",").head should equal ("localhost")
+              }
+              case _ => fail("Instance was not a ZookeeperInstance")
+            }
+          }
+
+          it("should use the username") {
+            scalaAccumulo.getUsername should equal ("root")
+          }
+
+          it("should use the password") {
+            scalaAccumulo.getPassword should equal ("secret")
+          }
+        }
+      } catch {
+        case e: java.net.SocketException => {
+          it("can not be run, as Accumulo doesn't appear to be running") (pending)
+        }
       }
-    }
+   }
+
+   describe("when the zookeeper connection fails") {
+     // TODO: need to figure out the best way to mock one of the companion methods
+     it("should give you a nice message")(pending)
+   }
   }
 }
 
